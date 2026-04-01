10d0d41f50f9186780d1ade037b39f8ee89264af
VitorHugoVaccari <luvaccari88@gmail.com>
2026-04-01 14:50:53 -0300

    conclui a atividade 6 repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let stringRepeat = str; 
+    for (let i = 1; i < num; i++) {
+        stringRepeat = stringRepeat + str;
+    }
+    return stringRepeat;
 };
 
-// Do not edit below this line
 module.exports = repeatString;
