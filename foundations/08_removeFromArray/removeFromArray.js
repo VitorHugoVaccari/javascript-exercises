ae6ccbe8e94e8e78cfea84595d705a2b3cde30d2
VitorHugoVaccari <luvaccari88@gmail.com>
2026-04-01 15:26:19 -0300

    Conclui a atividade 8 removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+    return arr.filter(function(item) {
+        return !nums.includes(item);
+    });
 };
 
 // Do not edit below this line
