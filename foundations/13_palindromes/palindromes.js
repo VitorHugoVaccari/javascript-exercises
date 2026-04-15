ed881282c0cd464b5cb446b731aff1fd25769bbc
VitorHugoVaccari <luvaccari88@gmail.com>
2026-04-15 14:39:18 -0300

    Conclui a atividade 13 Palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let arrOriginal = str.split('');
+    let arrOriginalSemVirgula = arrOriginal.filter(item => item !== ',' && item !== ' ');
+    let original = arrOriginalSemVirgula.join('')
+    let arrInvertido = arrOriginalSemVirgula.reverse();
+    let invertido = arrInvertido.join('');
 
+    if (invertido === original){
+        return true;
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
