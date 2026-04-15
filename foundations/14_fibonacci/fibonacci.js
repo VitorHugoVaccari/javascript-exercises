742e3d18bf865dd824d63092bb9cf1d55d8a72ec
VitorHugoVaccari <luvaccari88@gmail.com>
2026-04-15 17:01:21 -0300

    Conclui a atividade 14 Fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(contagem) {
+    let num;
+    if (typeof contagem !== "number") {
+        num = parseInt(contagem);
+    } else {
+        num = contagem;
+    }
 
+    if (num < 0) {
+        return "OOPS";
+    } else if (num == 0) {
+        return 0;
+    }
+
+    let anterior = 0;
+    let atual = 1 ;
+    let proximo = 0;
+    for (let i = 2; i <= num; i++) { 
+        proximo = anterior + atual;
+        anterior = atual;
+        atual = proximo;
+    }
+
+    return atual;
 };
 
 // Do not edit below this line
